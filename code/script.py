6a59a3c1264b5b582c930c5b3a34823264d76e58
Glitcher85 <44069760+Glitcher85@users.noreply.github.com>
2020-09-10 18:01:15 +0300

    A module that writes data when save.py isn't found in range of the main file

---
+++ code/script.py
@@ -0,0 +1,20 @@
+data = """mapping = {
+    "f1": "https://www.google.com",
+    "f2": "https://www.youtube.com",
+    "f3": "https://www.discord.com",
+    "f4": "https://www.twitch.tv",
+    "f5": "https://www.github.com",
+    "f6": "",
+    "f7": "",
+    "f8": "",
+    "f9": "",
+    "f10": "",
+    "f11": "",
+    "f12": "",
+    "home": "",
+    "end": "",
+    "delete": "",
+    "insert": "",
+    "pause": ""
+}
+"""
