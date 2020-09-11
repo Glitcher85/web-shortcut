7613992aef9f8a949b8ff36c067d8bc979ee3be6
Glitcher85 <44069760+Glitcher85@users.noreply.github.com>
2020-09-11 06:55:06 +0300

    Update save-opener.cs

--- code/save-opener.cs
+++ code/save-opener.cs
@@ -7,14 +7,14 @@ namespace SaveOpener {
         static void Main(string[] args) {
             string current_directory = Directory.GetCurrentDirectory();
             string destination = current_directory + @"\programs\save.py";
-	        string command = "/c notepad " + destination;
+	    string command = "/c notepad " + destination;
             if (File.Exists(destination)) {
                 Process.Start("cmd", command);
             } else {
                 Console.WriteLine("[ERROR]: An exception occured.");
-		        Console.WriteLine("Please make sure to open the main program at least once to create 'save.py' and do not change the directory name.\n");
-		        Console.WriteLine("Press [ENTER] to exit...");
-		        Console.ReadLine();
+		Console.WriteLine("Please make sure to open the main program at least once to create 'save.py' and do not change the directory name.\n");
+		Console.WriteLine("Press [ENTER] to exit...");
+		Console.ReadLine();
             }
         }
     }
